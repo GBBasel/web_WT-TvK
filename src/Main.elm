1c70f32fb31da51d1b3e11c6f7b7c753c15cd722
Baguet Fromasche Croisont <32932655+x8x-Timon-x8x@users.noreply.github.com>
2019-02-18 15:48:01 +0100

    Update Main.elm

--- src/Main.elm
+++ src/Main.elm
@@ -9,16 +9,26 @@ import Random exposing (int)
 
 -- MODEL
 
+type Screen
+    = Home
+    | Pattern
+    | Math
+    | InsertPattern
+
 type alias Model = 
     { difficulty : Int
     , pattern : List Char
-    , inputContent : String }
+    , inputContent : String
+    , screen : Screen
+    }
 
 initialModel : Model
 initialModel =
     { difficulty = 1
     , pattern = []
-    , inputContent = ""}
+    , inputContent = ""
+    , screen = Home
+    }
     
 
 init : () -> (Model, Cmd Msg)
@@ -33,8 +43,18 @@ type Msg
     | GeneratePattern (List Int)
     | Change String
     | Submit
+    | ChangeScreen
     
-
+nextscreen : Screen -> Screen
+nextscreen oldscreen = case oldscreen of
+    Home ->
+        Pattern
+    Pattern ->
+        Math
+    Math ->
+        InsertPattern
+    InsertPattern ->
+        Home
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
@@ -50,9 +70,12 @@ update msg model =
             ( { model | inputContent = newContent }, Cmd.none)
         Submit ->
             ( { model | difficulty = model.difficulty + increaseDifficulty }, Cmd.none )
+        ChangeScreen ->
+            ( { model | screen = nextscreen model.screen }, Cmd.none)
+        
         
 
-     
+      
       
 
 -- SUBSCRIPTIONS
@@ -67,17 +90,38 @@ subscriptions model =
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ text "Merke dir das folgende Muster:"
-        , p [] []
-        , text <| String.join "" <| List.map String.fromChar model.pattern
-        , p [] []
-        , button [ onClick Roll ] [ text "Roll" ]
-        , input [ placeholder "Text to reverse", value model.inputContent, onInput Change ] []
-        , div [] [ text model.inputContent ]
-        , button [ onClick Submit ] [ text "Submit" ]
-        , text <| String.fromInt model.difficulty
-        ]
+    case model.screen of
+        Home ->
+            div [][text "Home"
+                , p [] []
+                , button [ onClick ChangeScreen ] [ text "Spiel starten" ]
+                ]
+        Pattern ->
+            div []
+                [ text "Merke dir das folgende Muster:"
+                , p [] []
+                , text <| String.join "" <| List.map String.fromChar model.pattern
+                , p [] []
+                , button [ onClick Roll ] [ text "Roll" ]
+                , text <| String.fromInt model.difficulty
+                , p [] []
+                , button [ onClick ChangeScreen ] [ text "Weiter zum rechnen" ]
+                ]
+        Math ->
+            div [][text "Rechenspiel"
+                , p [] []
+                , button [ onClick ChangeScreen ] [ text "Weiter zum gemerkten Muster" ]
+                ]
+        InsertPattern ->
+            div [][text "Gib das Muster ein, welches du dir gemerkt hast"
+                , input [ placeholder "Text to reverse", value model.inputContent, onInput Change ] []
+                , div [] [ text model.inputContent ]
+                , button [ onClick Submit ] [ text "Submit" ]
+                , text <| String.fromInt model.difficulty
+                , p [] []
+                , button [ onClick ChangeScreen ] [ text "Zurück zum Home" ]
+                ]
+
 
 -- MAIN
 main : Program () Model Msg
@@ -86,4 +130,5 @@ main =
         { init = init
         , view = view
         , update = update
-        , subscriptions = subscriptions}
+        , subscriptions = subscriptions
+        }
